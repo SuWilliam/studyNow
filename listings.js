15f2515e65957f2ba0d4848b77a34fec4cea2f3f
Hon <kwokh@umich.edu>
2014-09-06 20:18:36 -0400

    added postsController

--- listings.js
+++ listings.js
@@ -2,14 +2,22 @@ var app = angular.module('studyNow', []);
 
 app.controller('postsController', function () {
 
-		this.title = ['Can anyone review at my essay?', 'Calc 2 study group!', 'Python help please!'];
-		this.location = ['Hatcher', 'UGLI', 'Duderstadt'];
-		this.course = ['ENG125', 'MATH116', 'PHYS140'];
-		this.note = ['Just need some opinions!', 'We\'re reviewing for the upcoming quiz!', 'I\m stuck on a tutorial.'];
-		this.map = ['', '', ''];
+	this.title = "";
+	this.name = "";
+	this.subject = ""; 
+	this.note = "";
+	this.latitude = "";
+	this.longitude = "";
 
-	this.getListing = function(){
-
+	this.getPosts = function(title, name, course, lat, lng, note){
+		this.title = title;
+		this.name = name;
+		this.subject = course; 
+		this.latitude = lat;
+		this.longitude = lng
+		this.note = note;
 	}
+
+this.getPosts();
 
 })
